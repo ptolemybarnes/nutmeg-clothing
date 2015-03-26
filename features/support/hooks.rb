515a987a955d35959eb458501560b342f4db6cb6
Ptolemy <ptolemybarnes@gmail.com>
2015-03-26 16:58:51 +0000

    The mock array of vouchers is now cleared between scenarios.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,7 @@
 Around do |scenario, block|
   Capybara.app.shopping_cart = ShoppingCart.new
+  Capybara.app.vouchers.clear
   block.call
   Capybara.app.shopping_cart = ShoppingCart.new
+  Capybara.app.vouchers.clear
 end
